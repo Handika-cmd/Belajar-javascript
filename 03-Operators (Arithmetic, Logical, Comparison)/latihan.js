18aa1ede72da8d2836e3eb419de1d2db3993931a
Handika Saputra <handikasaputra484@gmail.com>
2026-09-02 17:19:05 +0700

    learn: JavaScript

--- 03-Operators (Arithmetic, Logical, Comparison)/latihan.js
+++ 03-Operators (Arithmetic, Logical, Comparison)/latihan.js
@@ -1,6 +1,7 @@
 // ==== Latihan Membuat kalkulator === //
 
 /*1. aritmatic */
+console.log("=== Aritmatic ===")
 let angka = 17;
 let angka2 = 10;
 
@@ -10,3 +11,27 @@ console.log("Perkalian 17 * 10 =", angka * angka2 );
 console.log("Pembagian 17 / 10 =", angka / angka2);
 console.log("Bagi sisa dari 17 % 10 =", angka % angka2);
 
+// === COMPARISON OPERATOR ===
+console.log("\n\n --- COMPARISON ---")
+
+let a = 5;
+let b = "5";
+
+console.log("(5 == '5'): ", a == b, "-> (TRUE) Nilainya sama" );
+console.log("(5 === '5'): ", a === b, "-> (FALSE) Nilainya beda karna number vs string")
+
+
+
+//  === LOGICAL OPERATORS ===
+console.log("\n\n --- LOGICAL ---");
+
+let latihan = true;
+let tidaklatihan = false;
+
+// and &&
+console.log("Latihan dan sakit:", latihan && tidaklatihan); // "Keduanya harus true"
+// or ||
+console.log("Latihan dan Sehat: ", latihan || tidaklatihan); //"Salah satu harus tru (||)"
+
+// not !, Membalikkan kondisi
+console.log("latihan: ", !tidaklatihan)
