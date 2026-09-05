ff41ca41f1c1443a1861c29df1dd2674644544c0
Handika Saputra <handikasaputra484@gmail.com>
2026-09-05 15:42:54 +0700

    learn: Control FLow (If Else & Ternary)

--- 05-Control Flow (if else & ternary)/latihan.js
+++ 05-Control Flow (if else & ternary)/latihan.js
@@ -1,12 +1,22 @@
-const inputumur = prompt("Masukkan umur: ");
+const inputnama = prompt("Masukkan nama Siswa:");
+const inputnilai = prompt("Masukkan nilai 1-100");
 
-umur = Number(inputumur);
+nilai = Number(inputnilai);
 
-if (umur >= 17){
-    alert(`Boleh nonton karna umurmu ${umur}`)
-    console.log("Boleh nonton");
+if (nilai >= 88){
+    console.log(`Nama ${inputnama}, LULUS dengan nilai Sangat Baik (${nilai})`);
+    alert(`nama ${inputnama}, LULUS dengan nilai Sangat Baik (${nilai})`);
+}
+else if (nilai >= 80){
+    console.log(inputnama)
+    console.log(`Nama ${inputnama}, LULUS dengan nilai Baik (${nilai})`);
+    alert(`Nama ${inputnama}, LULUS dengan nilai Baik (${nilai})`);
+}
+else if (nilai >=75){
+    console.log(`Nama ${inputnama}, LULUS dengan nilai Cukup (${nilai})`);
+    alert(`Nama ${inputnama}, LULUS dengan nilai Cukup (${nilai})`);
 }
 else{
-    alert(`Maaf, umurmu ${umur} tahun tidak bisa nonton`)
-    console.log("Maaf, umur belum cukup");
+    console.log(`Nama ${inputnama}, TIDAK LULUS dengan nilai Kurang Cukup (${nilai})`);
+    alert(`Nama ${inputnama}, TIDAK LULUS dengan nilai Kurang Cukup (${nilai})`);
 }
